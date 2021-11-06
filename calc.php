0d40d25d90723418fe5ce0524ae4f3a06b19dca6
vhotsulenko <paraplan@ukr.net>
2021-11-06 17:10:09 +0200

    echo

--- calc.php
+++ calc.php
@@ -8,6 +8,7 @@ $point2 = clone $point1;
 $xb = $point2 ->x=7;
 $yb = $point2 ->y=8;
 $zb = $point2 ->z=9;
+echo "Расстояние между точками A($xa,$ya,$za) и В($xb,$yb,$zb): ";
 echo sqrt(pow(($xb  - $xa),2) + pow(($yb - $ya),2) + pow(($zb - $za),2));
 
 ?>
